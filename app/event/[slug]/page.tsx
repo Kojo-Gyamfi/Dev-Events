ed013f46f3fc2e07f08c0d652dc8bc17b5ed648a
Kojo-Gyamfi <emmanuelnanagyamfi@gmail.com>
2026-01-16 13:53:27 +0000

    Implement eventCard, server actions and caching

--- app/event/[slug]/page.tsx
+++ app/event/[slug]/page.tsx
@@ -155,7 +155,7 @@ const EventDetailsPage = async ({
         <div className="events">
           {similarEvents.length > 0 &&
             similarEvents.map((similarEvent: IEvent) => (
-              <EventCard key={similarEvent.id} {...similarEvent} />
+              <EventCard key={similarEvent.title} {...similarEvent} />
             ))}
         </div>
       </div>
